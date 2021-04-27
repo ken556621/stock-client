9ca5b9ae275b8651b9e05cb4011bcea5960954d7
Ken <ken.Yu@mile.cloud>
2021-04-27 15:31:44 +0800

    Add login form

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,27 +1,40 @@
 import { useForm } from "react-hook-form";
 
 import { makeStyles } from "@material-ui/core/styles";
+import Button from '@material-ui/core/Button';
 import Input from "@material-ui/core/Input";
 import InputLabel from "@material-ui/core/InputLabel";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import FormControl from "@material-ui/core/FormControl";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import Icon from "@material-ui/core/Icon";
-
+import EmailIcon from '@material-ui/icons/Email';
+import PersonIcon from '@material-ui/icons/Person';
+import LockIcon from '@material-ui/icons/Lock';
 
 
 const useLoginFormStyles = makeStyles((theme) => ({
     formWrapper: {
-        width: 100,
+        width: "50vw",
         maxWidth: 450,
-        borderRadius: 1,
+        padding: 30,
+        borderRadius: 8,
         boxShadow: "0 0 1.5rem #e6e6e6",
-        backgroundColor: "#fafafa"
+        backgroundColor: "#fafafa",
+        fontSize: 12,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    error: {
+        color: theme.palette.error.main
+    },
+    submitBtnRoot: {
+        display: "flex",
+        justifyContent: "flex-end"
     }
 }));
 
 const LoginForm = () => {
-    const { register, handleSubmit, errors } = useForm({
+    const { register, handleSubmit, formState: { errors } } = useForm({
         mode: "onChange"
     });
 
@@ -36,9 +49,7 @@ const LoginForm = () => {
             <form onSubmit={handleSubmit(onSubmit)} noValidate>
                 <FormControl
                     fullWidth
-                    required
                     margin="normal"
-                // error={emailError}
                 >
                     <InputLabel htmlFor="email">
                         Email
@@ -49,13 +60,66 @@ const LoginForm = () => {
                         name="email"
                         placeholder="Please Type Email"
                         autoFocus
+                        {...register("email", { required: true })}
                         startAdornment={
                             <InputAdornment position="start">
-                                <Icon>email</Icon>
+                                <EmailIcon />
                             </InputAdornment>
                         }
                     />
+                    {errors.email && <span className={classes.error}>Email is required</span>}
                 </FormControl>
+                <FormControl
+                    fullWidth
+                    margin="normal"
+                >
+                    <InputLabel htmlFor="email">
+                        Name
+                    </InputLabel>
+                    <Input
+                        id="name"
+                        type="name"
+                        name="name"
+                        placeholder="Please Type Name"
+                        autoFocus
+                        {...register("name", { required: true })}
+                        startAdornment={
+                            <InputAdornment position="start">
+                                <PersonIcon />
+                            </InputAdornment>
+                        }
+                    />
+                    {errors.name && <span className={classes.error}>Name is required</span>}
+                </FormControl>
+                <FormControl
+                    fullWidth
+                    margin="normal"
+                >
+                    <InputLabel htmlFor="email">
+                        Password
+                    </InputLabel>
+                    <Input
+                        id="password"
+                        type="password"
+                        name="password"
+                        placeholder="Please Type Password"
+                        autoFocus
+                        {...register("password", { required: true })}
+                        startAdornment={
+                            <InputAdornment position="start">
+                                <LockIcon />
+                            </InputAdornment>
+                        }
+                    />
+                    {errors.password && <span className={classes.error}>Password is required</span>}
+                </FormControl>
+                <div className={classes.submitBtnRoot}>
+                    <Button
+                        type="submit"
+                    >
+                        Submit
+                    </Button>
+                </div>
             </form>
         </div>
     )
